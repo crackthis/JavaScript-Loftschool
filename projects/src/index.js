29942ef75e258540c8895a9885ebb3ba775a55e2
CrackThis <prm1078life@gmail.com>
2020-10-07 09:16:04 +0200

    homework_5

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,244 +1,81 @@
-/* ДЗ 4 - работа с DOM */
+/*
+ Страница должна предварительно загрузить список городов из
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ и отсортировать в алфавитном порядке.
+
+ При вводе в текстовое поле, под ним должен появляться список тех городов,
+ в названии которых, хотя бы частично, есть введенное значение.
+ Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
+
+ Во время загрузки городов, на странице должна быть надпись "Загрузка..."
+ После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
+
+ Разметку смотрите в файле towns.html
+
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+
+ *** Часть со звездочкой ***
+ Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
+ то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
+ При клике на кнопку, процесс загрузки повторяется заново
+ */
 
 /*
- Задание 1:
-
- 1.1: Функция должна создать элемент с тегом DIV
-
- 1.2: В созданный элемент необходимо поместить текст, переданный в параметр text
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
 
  Пример:
-   createDivWithText('loftschool') // создаст элемент div, поместит в него 'loftschool' и вернет созданный элемент
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
  */
-function createDivWithText(text) {
-  const newElem = document.createElement('div');
-  newElem.textContent = text;
-  return newElem;
+
+import './towns.html';
+
+const homeworkContainer = document.querySelector('#homework-container');
+
+/*
+ Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ */
+function loadTowns() {
 }
 
 /*
- Задание 2:
-
- Функция должна вставлять элемент, переданный в параметре what в начало элемента, переданного в параметре where
-
- Пример:
-   prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
- */
-function prepend(what, where) {
-  return where.prepend(what);
-}
-
-/*
- Задание 3:
-
- 3.1: Функция должна перебрать все дочерние элементы узла, переданного в параметре where
-
- 3.2: Функция должна вернуть массив, состоящий из тех дочерних элементов следующим соседом которых является элемент с тегом P
+ Функция должна проверять встречается ли подстрока chunk в строке full
+ Проверка должна происходить без учета регистра символов
 
  Пример:
-   Представим, что есть разметка:
-   <body>
-      <div></div>
-      <p></p>
-      <a></a>
-      <span></span>
-      <p></p>
-   </body>
-
-   findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих элементов является элемент с тегом P
+   isMatching('Moscow', 'moscow') // true
+   isMatching('Moscow', 'mosc') // true
+   isMatching('Moscow', 'cow') // true
+   isMatching('Moscow', 'SCO') // true
+   isMatching('Moscow', 'Moscov') // false
  */
-function findAllPSiblings(where) {
-  let elems = [];
-  for (const elem of where.children) {
-    if(elem.nextElementSibling && elem.nextElementSibling.tagName === 'P'){
-      elems.push(elem);
-    }
-  }
-  return elems;
+function isMatching(full, chunk) {
 }
 
-console.log(findAllPSiblings(document.body));
+/* Блок с надписью "Загрузка" */
+const loadingBlock = homeworkContainer.querySelector('#loading-block');
+/* Блок с надписью "Не удалось загрузить города" и кнопкой "Повторить" */
+const loadingFailedBlock = homeworkContainer.querySelector('#loading-failed');
+/* Кнопка "Повторить" */
+const retryButton = homeworkContainer.querySelector('#retry-button');
+/* Блок с текстовым полем и результатом поиска */
+const filterBlock = homeworkContainer.querySelector('#filter-block');
+/* Текстовое поле для поиска по городам */
+const filterInput = homeworkContainer.querySelector('#filter-input');
+/* Блок с результатами поиска */
+const filterResult = homeworkContainer.querySelector('#filter-result');
 
-/*
- Задание 4:
+retryButton.addEventListener('click', () => {
+});
 
- Функция представленная ниже, перебирает все дочерние узлы типа "элемент" внутри узла переданного в параметре where и возвращает массив из текстового содержимого найденных элементов
- Но похоже, что в код функции закралась ошибка и она работает не так, как описано.
+filterInput.addEventListener('input', function () {
+});
 
- Необходимо найти и исправить ошибку в коде так, чтобы функция работала так, как описано выше.
+loadingFailedBlock.classList.add('hidden');
+filterBlock.classList.add('hidden');
 
- Пример:
-   Представим, что есть разметка:
-   <body>
-      <div>привет</div>
-      <div>loftschool</div>
-   </body>
-
-   findError(document.body) // функция должна вернуть массив с элементами 'привет' и 'loftschool'
- */
-function findError(where) {
-  const result = [];
-
-  for (const child of where.children) {
-    if(child.tagName !== 'SCRIPT') result.push(child.textContent);
-  }
-
-  return result;
-}
-
-/*
- Задание 5:
-
- Функция должна перебрать все дочерние узлы элемента переданного в параметре where и удалить из него все текстовые узлы
-
- Задачу необходимо решить без использования рекурсии, то есть можно не уходить вглубь дерева.
- Так же будьте внимательны при удалении узлов, т.к. можно получить неожиданное поведение при переборе узлов
-
- Пример:
-   После выполнения функции, дерево <div></div>привет<p></p>loftchool!!!
-   должно быть преобразовано в <div></div><p></p>
- */
-function deleteTextNodes(where) {
-  for (const child of where.childNodes) {
-    if(child.nodeType == 3) {
-      child.remove();
-    }
-  }
-}
-/*
- Задание 6:
-
- Выполнить предыдущее задание с использование рекурсии - то есть необходимо заходить внутрь каждого дочернего элемента (углубляться в дерево)
-
- Будьте внимательны при удалении узлов, т.к. можно получить неожиданное поведение при переборе узлов
-
- Пример:
-   После выполнения функции, дерево
-   <span><div><b></b></div><p>loftchool</p><b>99999</b></span>
-   должно быть преобразовано в <span><div><b></b></div><p></p></span>
- */
-function deleteTextNodesRecursive(where) {
-  for(const child of [...where.childNodes]) {
-    if(child.nodeType === 3) {
-      child.remove();
-    } else if (child.nodeType === 1 && child.childNodes.length > 0) {
-      deleteTextNodesRecursive(child);
-    }
-  }
-}
-
-/*
- Задание 7 *:
-
- Необходимо собрать статистику по всем узлам внутри элемента переданного в параметре root и вернуть ее в виде объекта
- Статистика должна содержать:
- - количество текстовых узлов
- - количество элементов каждого класса
- - количество элементов каждого тега
- Для работы с классами рекомендуется использовать classList
- Постарайтесь не создавать глобальных переменных
-
- Пример:
-   Для дерева <div class="some-class-1"><b>привет!</b> <b class="some-class-1 some-class-2">loftschool</b></div>
-   должен быть возвращен такой объект:
-   {
-     tags: { DIV: 1, B: 2},
-     classes: { "some-class-1": 2, "some-class-2": 1 },
-     texts: 3
-   }
- */
-function collectDOMStat(root) {
-  let stat = {
-    tags: {},
-    classes: {},
-    texts: 0,
-  }
-  function statFunc(root) {
-    for(const child of root.childNodes) {
-      if(child.nodeType === 3) {
-        stat.texts++;
-      } else if (child.nodeType === 1) {
-        if (child.tagName in stat.tags) {
-          stat.tags[child.tagName]++;
-        } else {
-          stat.tags[child.tagName] = 1;
-        }
-        for (const className of child.classList) {
-          if (className in stat.classes) {
-            stat.classes[className]++;
-          } else {
-            stat.classes[className] = 1;
-          }
-        }
-        statFunc(child);
-      }
-    }
-  }
-
-  statFunc(root);
-
-  return stat;
-}
-
-/*
- Задание 8 *:
-
- 8.1: Функция должна отслеживать добавление и удаление элементов внутри элемента переданного в параметре where
- Как только в where добавляются или удаляются элементы,
- необходимо сообщать об этом при помощи вызова функции переданной в параметре fn
-
- 8.2: При вызове fn необходимо передавать ей в качестве аргумента объект с двумя свойствами:
-   - type: типа события (insert или remove)
-   - nodes: массив из удаленных или добавленных элементов (в зависимости от события)
-
- 8.3: Отслеживание должно работать вне зависимости от глубины создаваемых/удаляемых элементов
-
- Рекомендуется использовать MutationObserver
-
- Пример:
-   Если в where или в одного из его детей добавляется элемент div
-   то fn должна быть вызвана с аргументом:
-   {
-     type: 'insert',
-     nodes: [div]
-   }
-
-   ------
-
-   Если из where или из одного из его детей удаляется элемент div
-   то fn должна быть вызвана с аргументом:
-   {
-     type: 'remove',
-     nodes: [div]
-   }
- */
-function observeChildNodes(where, fn) {
-  let mutationObserver = new MutationObserver((mutations) => {
-    mutations.forEach(e => {
-      if(e.type === 'childList') {
-        fn({
-          type: e.addedNodes.length ? 'insert' : 'remove',
-          nodes: [
-              ...(e.addedNodes.length ? e.addedNodes : e.removedNodes),
-          ],
-        });
-      }
-    });
-  });
-
-  mutationObserver.observe(where, {
-    childList: true,
-    subtree: true,
-  });
-}
-
-export {
-  createDivWithText,
-  prepend,
-  findAllPSiblings,
-  findError,
-  deleteTextNodes,
-  deleteTextNodesRecursive,
-  collectDOMStat,
-  observeChildNodes,
-};
+export { loadTowns, isMatching };
--- projects/src/index.spec.js
+++ projects/src/index.spec.js
@@ -1,240 +1,82 @@
-import { randomValue } from '../../scripts/helper';
-import {
-  collectDOMStat,
-  createDivWithText,
-  deleteTextNodes,
-  deleteTextNodesRecursive,
-  findAllPSiblings,
-  findError,
-  observeChildNodes,
-  prepend,
-} from './index';
+const hasOwnProperty = Object.prototype.hasOwnProperty;
 
-function random(type) {
-  const result = randomValue(type);
+describe('ДЗ 6.2 - Фильтр городов', () => {
+  const filterPage = require('./index');
+  const homeworkContainer = document.querySelector('#homework-container');
+  let loadingBlock;
+  let filterBlock;
+  let filterInput;
+  let filterResult;
 
-  if (type === 'string') {
-    return encodeURIComponent(result);
-  }
+  beforeAll(() => filterPage.loadTowns());
 
-  return result;
-}
-
-describe('ДЗ 4 - Работа с DOM', () => {
-  describe('createDivWithText', () => {
-    it('должна возвращать элемент с тегом DIV', () => {
-      const text = random('string');
-      const result = createDivWithText(text);
-
-      expect(result).toBeInstanceOf(Element);
-      expect(result.tagName).toBe('DIV');
+  describe('Функциональное тестирование', () => {
+    describe('isMatching', () => {
+      it('должна определять присутствие подстроки в строке', () => {
+        expect(filterPage.isMatching('Moscow', 'moscow'));
+        expect(filterPage.isMatching('Moscow', 'mos'));
+        expect(filterPage.isMatching('Moscow', 'MoS'));
+        expect(filterPage.isMatching('Moscow', 'cow'));
+        expect(!filterPage.isMatching('Moscow', 'Berlin'));
+      });
     });
+    describe('loadTowns', () => {
+      it('должна возвращать Promise', () => {
+        const result = filterPage.loadTowns();
+        expect(result).toBeInstanceOf(Promise);
+      });
 
-    it('должна добавлять текст в элемент', () => {
-      const text = random('string');
-      const result = createDivWithText(text);
+      it('Promise должен разрешаться массивом из городов', (done) => {
+        /* eslint-disable max-nested-callbacks */
+        const result = filterPage.loadTowns();
 
-      expect(result.textContent).toBe(text);
-    });
-  });
+        result
+          .then((towns) => {
+            expect(Array.isArray(towns));
+            expect(towns.length).toBe(50);
+            towns.forEach((town, i, towns) => {
+              expect(hasOwnProperty.call(town, 'name'));
 
-  describe('prepend', () => {
-    it('должна добавлять элемент в начало', () => {
-      const where = document.createElement('div');
-      const what = document.createElement('p');
-      const whereText = random('string');
-      const whatText = random('string');
-
-      where.innerHTML = `, <b>${whereText}</b>!`;
-      what.textContent = whatText;
-
-      prepend(what, where);
-
-      expect(where.firstChild).toBe(what);
-      expect(where.innerHTML).toBe(`<p>${whatText}</p>, <b>${whereText}</b>!`);
+              if (i) {
+                expect(towns[i - 1].name.localeCompare(town.name)).toBeLessThanOrEqual(0);
+              }
+            });
+            done();
+          })
+          .catch(done);
+      });
     });
   });
 
-  describe('findAllPSiblings', () => {
-    it('должна возвращать массив с элементами, соседями которых являются P', () => {
-      const where = document.createElement('div');
+  describe('Интеграционное тестирование', () => {
+    it('на старнице должны быть элементы с нужными id', () => {
+      loadingBlock = homeworkContainer.querySelector('#loading-block');
+      filterBlock = homeworkContainer.querySelector('#filter-block');
+      filterInput = homeworkContainer.querySelector('#filter-input');
+      filterResult = homeworkContainer.querySelector('#filter-result');
 
-      where.innerHTML = '<div></div><p></p><span></span><span></span><p></p>';
-      const result = findAllPSiblings(where);
-
-      expect(Array.isArray(result));
-      expect(result).toEqual([where.children[0], where.children[3]]);
+      expect(loadingBlock).toBeInstanceOf(Element);
+      expect(filterBlock).toBeInstanceOf(Element);
+      expect(filterInput).toBeInstanceOf(Element);
+      expect(filterResult).toBeInstanceOf(Element);
     });
-  });
 
-  describe('findError', () => {
-    it('должна возвращать массив из текстового содержимого элементов', () => {
-      const where = document.createElement('div');
-      const text1 = random('string');
-      const text2 = random('string');
-
-      where.innerHTML = ` <div>${text1}</div>, <div>${text2}</div>!!!`;
-      const result = findError(where);
-
-      expect(Array.isArray(result));
-      expect(result).toEqual([text1, text2]);
-    });
-  });
-
-  describe('deleteTextNodes', () => {
-    it('должна удалить все текстовые узлы', () => {
-      const where = document.createElement('div');
-
-      where.innerHTML = ` <div></div>${random('string')}<p></p>${random('string')}`;
-      deleteTextNodes(where);
-
-      expect(where.innerHTML).toBe('<div></div><p></p>');
-    });
-  });
-
-  describe('deleteTextNodesRecursive', () => {
-    it('должна рекурсивно удалить все текстовые узлы', () => {
-      const where = document.createElement('div');
-      const text1 = random('string');
-      const text2 = random('string');
-      const text3 = random('string');
-
-      where.innerHTML = `<span> <div> <b>${text1}</b> </div> <p>${text2}</p> ${text3}</span>`;
-      deleteTextNodesRecursive(where);
-
-      expect(where.innerHTML).toBe('<span><div><b></b></div><p></p></span>');
-    });
-  });
-
-  describe('collectDOMStat', () => {
-    it('должна вернуть статистику по переданному дереву', () => {
-      const where = document.createElement('div');
-      const class1 = `class-${random('number')}`;
-      const class2 = `class-${random('number')}-${random('number')}`;
-      const text1 = random('string');
-      const text2 = random('string');
-      const stat = {
-        tags: { P: 1, B: 2 },
-        classes: { [class1]: 2, [class2]: 1 },
-        texts: 3,
-      };
-
-      where.innerHTML = `<p class="${class1}"><b>${text1}</b> <b class="${class1} ${class2}">${text2}</b></p>`;
-      const result = collectDOMStat(where);
-
-      expect(result).toEqual(stat);
-    });
-  });
-
-  describe('observeChildNodes', () => {
-    it('должна вызывать fn при добавлении элементов в указанный элемент', (done) => {
-      const where = document.createElement('div');
-      const fn = (info) => {
-        expect(typeof info === 'object' && info && info.constructor === 'Object');
-        expect(info.type).toBe(targetInfo.type);
-        expect(Array.isArray(info.nodes));
-        expect(info.nodes.length).toBe(targetInfo.nodes.length);
-        expect(targetInfo.nodes).toEqual(info.nodes);
+    it('должен показываться список городов, соответствующих фильтру', (done) => {
+      filterInput.value = 'fr';
+      filterInput.dispatchEvent(new KeyboardEvent('input'));
+      setTimeout(() => {
+        expect(filterResult.children.length).toBe(3);
         done();
-      };
-      const elementToInsert = document.createElement('div');
-      const targetInfo = {
-        type: 'insert',
-        nodes: [elementToInsert],
-      };
-
-      document.body.appendChild(where);
-
-      observeChildNodes(where, fn);
-      where.appendChild(elementToInsert);
-
-      document.body.removeChild(where);
+      }, 1000);
     });
 
-    it('должна вызывать fn при добавлении множества элементов в указанный элемент', (done) => {
-      const where = document.createElement('div');
-      const fn = (info) => {
-        expect(typeof info === 'object' && info && info.constructor === 'Object');
-        expect(info.type).toBe(targetInfo.type);
-        expect(Array.isArray(info.nodes));
-        expect(info.nodes.length).toBe(targetInfo.nodes.length);
-        expect(targetInfo.nodes).toEqual(info.nodes);
+    it('результат должен быть пуст, если в поле пусто', (done) => {
+      filterInput.value = '';
+      filterInput.dispatchEvent(new KeyboardEvent('input'));
+      setTimeout(() => {
+        expect(filterResult.children.length).toBe(0);
         done();
-      };
-      const elementToInsert1 = document.createElement('div');
-      const elementToInsert2 = document.createElement('div');
-      const elementToInsert3 = document.createElement('div');
-      const targetInfo = {
-        type: 'insert',
-        nodes: [elementToInsert1, elementToInsert2, elementToInsert3],
-      };
-      const fragment = new DocumentFragment();
-
-      document.body.appendChild(where);
-
-      fragment.appendChild(elementToInsert1);
-      fragment.appendChild(elementToInsert2);
-      fragment.appendChild(elementToInsert3);
-
-      observeChildNodes(where, fn);
-      where.appendChild(fragment);
-
-      document.body.removeChild(where);
-    });
-
-    it('должна вызывать fn при удалении элементов из указанного элемента', (done) => {
-      const where = document.createElement('div');
-      const fn = (info) => {
-        expect(typeof info === 'object' && info && info.constructor === 'Object');
-        expect(info.type).toBe(targetInfo.type);
-        expect(Array.isArray(info.nodes));
-        expect(info.nodes.length).toBe(targetInfo.nodes.length);
-        expect(targetInfo.nodes).toEqual(info.nodes);
-        done();
-      };
-      const elementToRemove = document.createElement('div');
-      const targetInfo = {
-        type: 'remove',
-        nodes: [elementToRemove],
-      };
-
-      document.body.appendChild(where);
-
-      where.appendChild(elementToRemove);
-      observeChildNodes(where, fn);
-      where.removeChild(elementToRemove);
-
-      document.body.removeChild(where);
-    });
-
-    it('должна вызывать fn при удалении множества элементов из указанного элемента', (done) => {
-      const where = document.createElement('div');
-      const fn = (info) => {
-        expect(typeof info === 'object' && info && info.constructor === 'Object');
-        expect(info.type).toBe(targetInfo.type);
-        expect(Array.isArray(info.nodes));
-        expect(info.nodes.length).toBe(targetInfo.nodes.length);
-        expect(targetInfo.nodes).toEqual(info.nodes);
-        done();
-      };
-      const elementToRemove1 = document.createElement('div');
-      const elementToRemove2 = document.createElement('div');
-      const elementToRemove3 = document.createElement('div');
-      const targetInfo = {
-        type: 'remove',
-        nodes: [elementToRemove1, elementToRemove2, elementToRemove3],
-      };
-
-      document.body.appendChild(where);
-
-      where.appendChild(elementToRemove1);
-      where.appendChild(elementToRemove2);
-      where.appendChild(elementToRemove3);
-
-      observeChildNodes(where, fn);
-      where.innerHTML = '';
-
-      document.body.removeChild(where);
+      }, 1000);
     });
   });
 });
