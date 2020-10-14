8f9613dadb61b5293ffa592dcdf4309c4e5662e0
CrackThis <prm1078life@gmail.com>
2020-10-14 02:30:35 +0200

    homework_7

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,41 +1,55 @@
-/* Задание со звездочкой */
-
 /*
- Создайте страницу с кнопкой.
- При нажатии на кнопку должен создаваться div со случайными размерами, цветом и позицией на экране
- Необходимо предоставить возможность перетаскивать созданные div при помощи drag and drop
+ ДЗ 7 - Создать редактор cookie с возможностью фильтрации
+
+ 7.1: На странице должна быть таблица со списком имеющихся cookie. Таблица должна иметь следующие столбцы:
+   - имя
+   - значение
+   - удалить (при нажатии на кнопку, выбранная cookie удаляется из браузера и таблицы)
+
+ 7.2: На странице должна быть форма для добавления новой cookie. Форма должна содержать следующие поля:
+   - имя
+   - значение
+   - добавить (при нажатии на кнопку, в браузер и таблицу добавляется новая cookie с указанным именем и значением)
+
+ Если добавляется cookie с именем уже существующей cookie, то ее значение в браузере и таблице должно быть обновлено
+
+ 7.3: На странице должно быть текстовое поле для фильтрации cookie
+ В таблице должны быть только те cookie, в имени или значении которых, хотя бы частично, есть введенное значение
+ Если в поле фильтра пусто, то должны выводиться все доступные cookie
+ Если добавляемая cookie не соответствует фильтру, то она должна быть добавлена только в браузер, но не в таблицу
+ Если добавляется cookie, с именем уже существующей cookie и ее новое значение не соответствует фильтру,
+ то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
+
  Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
  */
 
+import './cookie.html';
+
 /*
- homeworkContainer - это контейнер для всех ваших домашних заданий
+ app - это контейнер для всех ваших домашних заданий
  Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
+
  Пример:
    const newDiv = document.createElement('div');
    homeworkContainer.appendChild(newDiv);
  */
-import './dnd.html';
+const homeworkContainer = document.querySelector('#homework-container');
+// текстовое поле для фильтрации cookie
+const filterNameInput = homeworkContainer.querySelector('#filter-name-input');
+// текстовое поле с именем cookie
+const addNameInput = homeworkContainer.querySelector('#add-name-input');
+// текстовое поле со значением cookie
+const addValueInput = homeworkContainer.querySelector('#add-value-input');
+// кнопка "добавить cookie"
+const addButton = homeworkContainer.querySelector('#add-button');
+// таблица со списком cookie
+const listTable = homeworkContainer.querySelector('#list-table tbody');
 
-const homeworkContainer = document.querySelector('#app');
-
-document.addEventListener('mousemove', (e) => {
+filterNameInput.addEventListener('input', function () {
 });
 
-export function createDiv() {
-    const newDiv = document.createElement("div");
-    newDiv.height = (Math.random() * 100);
-    newDiv.width = (Math.random() * 100);
-    newDiv.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255} )`;
-    newDiv.className = '.draggable-div';
-    newDiv.top = Math.random() * 200;
-    newDiv.bottom = Math.random() * 200;
-    newDiv.left = Math.random() * 200;
-    newDiv.right = Math.random() * 200;
-    homeworkContainer.appendChild(newDiv);
-}
+addButton.addEventListener('click', () => {
+});
 
-const addDivButton = homeworkContainer.querySelector('#addDiv');
-
-addDivButton.addEventListener('click', function () {
-    createDiv();
+listTable.addEventListener('click', (e) => {
 });
